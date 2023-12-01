d2d43ef572d4d639691a9f1618a61c0e7e5aa561
zhouyongcai <807923011@qq.com>
2023-12-01 10:23:27 +0800

    第1次提交

--- 02_intermediate/frame_window/test_frame.py
+++ 02_intermediate/frame_window/test_frame.py
@@ -6,7 +6,6 @@
 from selenium.webdriver.common.by import By
 from frame_window.base import Base
 
-
 class TestFrame(Base):
     def test_frame(self):
         self.driver.get("https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable")
